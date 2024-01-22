d9b79388a6c58fa4d500ec2039ded2267cab06ab
burgundythedev <bourgogne.olivier@gmail.com>
2024-01-22 23:47:54 +0100

    :construction: adding menu and logo to header

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,11 +1,63 @@
-import logo from "../assets/images/header-logo.svg"
-import burger from "../assets/icons/hamburger.svg"
+import logo from "../assets/images/logo.png";
+import burger from "../assets/icons/hamburger.svg";
 const Header = () => {
   return (
-    <header className="">
-      <nav>
-        <a href=""><img src={logo} alt="logo-website"/></a>
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img
+            width={200}
+            height={100}
+            src={logo}
+            alt="logo-website"
+          />
+        </a>
         <a />
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          <li>
+            <a
+              href="/"
+              className="fonts-montserrat leading-normal text-lg text-slate-gray"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              className="fonts-montserrat leading-normal text-lg text-slate-gray"
+            >
+              About us
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              className="fonts-montserrat leading-normal text-lg text-slate-gray"
+            >
+              Products
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              className="fonts-montserrat leading-normal text-lg text-slate-gray"
+            >
+              Contact us
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              className="fonts-montserrat leading-normal text-lg text-slate-gray"
+            >
+              Home
+            </a>
+          </li>
+        </ul>
+        <div className="hidden max-lg:block">
+          <img src={burger} width={25} height={25} alt="burgermenu-logo" />
+        </div>
       </nav>
     </header>
   );
