9bec05362b948d06c7f767fbeb2cd834ae08319e
burgundythedev <bourgogne.olivier@gmail.com>
2024-01-25 13:00:00 +0100

    :construction: hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,33 @@
-
+import Button from "../Layout/Button";
+import arrowRight  from "../assets/icons/arrow-right.svg";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28">
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer collections
+        </p>
 
-export default Hero
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            New Arrivals
+          </span>
+          <br />
+          <span className="text-coral-red inline-block mt-3">All Brands</span>
+        </h1>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+
+        <Button label="Shop now" iconURL={arrowRight} />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
